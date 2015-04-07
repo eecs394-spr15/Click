b4eafe6c23a11983c83b4d0eebac8bfaab4e2533
hzn942 <hzn942@gmail.com>
2015-04-07 13:03:58 -0500

    fix save new event problem, still have bug

--- app/event/index.js
+++ app/event/index.js
@@ -1,3 +1,4 @@
+Parse.initialize("6PkS9v12fGDnNYc1dQrwQOH6l2ilnDQ3TrLtCKaN", "rcZtT4MfyGOiKkUNBApCYhY3lh3hhbw6N5Vyhzi0");
 angular.module('event', [
   /* Declare any module-specific dependencies here */
   //'common',
--- app/event/scripts/NewController.js
+++ app/event/scripts/NewController.js
@@ -6,9 +6,23 @@ angular
     $scope.submitForm = function () {
       $scope.showSpinner = true;
       newevent = new Event($scope.event);
-      newevent.save().then( function () {
+
+      var TestObject = Parse.Object.extend("Events");
+      var testObject = new TestObject();
+      testObject.save($scope.event, {
+      success: function(object) {
         supersonic.ui.modal.hide();
+      },
+      error: function(model, error) {
+        steroids.logger.log(error);
+      }
       });
+
+
+
+      // newevent.save().then( function () {
+      //   supersonic.ui.modal.hide();
+      // });
     };
 
     $scope.cancel = function () {
