a32cdc1aaa2ad5d8919f70448694fe4e96ba006b
tommyshieh <tommyshieh@hotmail.com>
2015-04-19 19:06:44 -0500

    add up/down vote

--- app/event/scripts/IndexController.js
+++ app/event/scripts/IndexController.js
@@ -13,4 +13,52 @@ angular
     });
 
     $scope.predicate = "DateTime";
+    
+    $scope.up = function (id) {
+
+      
+  
+      var Events = Parse.Object.extend("Events");
+      var query = new Parse.Query(Events);
+      query.get(id.toString(), {
+        success: function(event) {
+          // The object was retrieved successfully.
+          event.increment('Vote');
+          event.save();
+          alert(event.get('Vote'));
+          location.reload();
+
+
+        },
+        error: function(object, error) {
+          // The object was not retrieved successfully.
+          // error is a Parse.Error with an error code and message.
+          alert('vote error');
+        }
+      });  
+    };
+    $scope.down = function (id) {  
+      var Events = Parse.Object.extend("Events");
+      var query = new Parse.Query(Events);
+      query.get(id.toString(), {
+        success: function(event) {
+          // The object was retrieved successfully.
+          event.increment('Vote',-1);
+          event.save();
+          alert(event.get('Vote'));
+          location.reload();
+
+        },
+        error: function(object, error) {
+          // The object was not retrieved successfully.
+          // error is a Parse.Error with an error code and message.
+          alert('vote error');
+        }
+      });  
+
+  
+     
+  
+    };
+
   });
